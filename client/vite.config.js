d0e3fce9805830ad523908461d1887dd04d00ee5
Joshua <adrishpinto423@gmail.com>
2025-04-11 10:22:34 +0530

    new route config

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: 'routes.json', 
+          src: 'staticwebapp.config.json file', 
           dest: ''         
         }
       ]
